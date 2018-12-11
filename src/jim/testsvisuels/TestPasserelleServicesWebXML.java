16f1e1114c572237872b926bb7ff989a79aefb42
Shobu <shobu13@hotmail.fr>
2018-12-11 16:52:25 +0100

    Revert "Auto stash before merge of "master" and "origin/master""
    This reverts commit 7d71ff17e462329e1395a121f02473b1751a471a.

--- src/jim/testsvisuels/TestPasserelleServicesWebXML.java
+++ src/jim/testsvisuels/TestPasserelleServicesWebXML.java
@@ -12,58 +12,45 @@ import jim.classes.Utilisateur;
 
 public class TestPasserelleServicesWebXML {
 
-    public static void main(String[] args) throws ParseException {
-
-        String msg;
-
-        // test visuel de la méthode getTousLesUtilisateurs
-
-
-        //<editor-fold desc="test getLesUtilisateursQueJautorise">
-        /*// test visuel de la méthode getLesUtilisateursQueJautorise
-        ArrayList<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
-        msg = PasserelleServicesWebXML.getLesUtilisateursQueJautorise("europa", Outils.sha1("mdputilisateur"), lesUtilisateurs);
-        // affichage de la réponse
-        System.out.println(msg);
-        // affichage du nombre d'utilisateurs
-        System.out.println("Nombre d'utilisateurs : " + lesUtilisateurs.size());
-        // affichage de tous les utilisateurs
-        for (Utilisateur unUtilisateur : lesUtilisateurs) {
-            System.out.println(unUtilisateur.toString());
-        }*/
-        //</editor-fold>
+	public static void main(String[] args) throws ParseException {
+		
+		String msg;
+	
+		// test visuel de la méthode getTousLesUtilisateurs
 
 
-        //<editor-fold desc="test getLesUtilisateursQuiMautorisent">
-        //// test visuel de la méthode getLesUtilisateursQuiMautorisent
-        //ArrayList<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
-        //msg = PasserelleServicesWebXML.getLesUtilisateursQuiMautorisent("europa", Outils.sha1("mdputilisateur"), lesUtilisateurs);
-        //// affichage de la réponse
-        //System.out.println(msg);
-        //// affichage du nombre d'utilisateurs
-        //System.out.println("Nombre d'utilisateurs : " + lesUtilisateurs.size());
-        //// affichage de tous les utilisateurs
-        //for (Utilisateur unUtilisateur : lesUtilisateurs)
-        //{	System.out.println(unUtilisateur.toString());
-        //}
-        //</editor-fold>
+		// test visuel de la méthode getLesUtilisateursQueJautorise
+		ArrayList<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
+		msg = PasserelleServicesWebXML.getLesUtilisateursQueJautorise("europa", Outils.sha1("mdputilisateur"), lesUtilisateurs);
+		// affichage de la réponse
+		System.out.println(msg);
+		// affichage du nombre d'utilisateurs
+		System.out.println("Nombre d'utilisateurs : " + lesUtilisateurs.size());
+		// affichage de tous les utilisateurs
+		for (Utilisateur unUtilisateur : lesUtilisateurs)
+		{	System.out.println(unUtilisateur.toString());
+		}
 
 
-        // test visuel de la méthode getLesParcoursDunUtilisateur
 
+		//// test visuel de la méthode getLesUtilisateursQuiMautorisent
+		//ArrayList<Utilisateur> lesUtilisateurs = new ArrayList<Utilisateur>();
+		//msg = PasserelleServicesWebXML.getLesUtilisateursQuiMautorisent("europa", Outils.sha1("mdputilisateur"), lesUtilisateurs);
+		//// affichage de la réponse
+		//System.out.println(msg);
+		//// affichage du nombre d'utilisateurs
+		//System.out.println("Nombre d'utilisateurs : " + lesUtilisateurs.size());
+		//// affichage de tous les utilisateurs
+		//for (Utilisateur unUtilisateur : lesUtilisateurs)
+		//{	System.out.println(unUtilisateur.toString());
+		//}
 
-        // test visuel de la méthode getUnParcoursEtSesPoints
+		
+		// test visuel de la méthode getLesParcoursDunUtilisateur
 
-        //<editor-fold desc="test getUnParcoursEtSesPoints">
-        // test visuel de la méthode getUnParcoursEtSesPoints
-        Trace laTrace = new Trace();
-        msg = PasserelleServicesWebXML.getUnParcoursEtSesPoints("europa", Outils.sha1("mdputilisateur"), 2, laTrace);
-        // affichage de la réponse
-        System.out.println(msg);
-        // affichage de la trace
-        System.out.println(laTrace.toString());
-        //</editor-fold>
+		
+		// test visuel de la méthode getUnParcoursEtSesPoints
 
-
-    } // fin Main
+	
+	} // fin Main
 } // fin class
